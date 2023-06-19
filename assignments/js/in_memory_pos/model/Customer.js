76880bedeaea829609f3440fc973eeba4ec78865
dinithimendis <dinithimendis42@gmail.com>
2023-06-19 13:07:03 +0530

    in_pos_memory update

---
+++ assignments/js/in_memory_pos/model/Customer.js
@@ -0,0 +1,8 @@
+class Customer {
+    constructor(id, name, address, contact) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+    }
+}
---
+++ assignments/js/in_memory_pos/model/Item.js
@@ -0,0 +1,8 @@
+class Item {
+    constructor(itemCode, itemName, qty, unitPrice) {
+        this.itemCode = itemCode;
+        this.itemName = itemName;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+}
